90f248d0215c7120018720d2f8a06bd2996bf8db
StefanGan <stefangan96@gmail.com>
2021-01-04 10:14:29 +0800

    Files Added

---
+++ app/src/main/java/br/com/aramizu/themoviedb/data/model/MoviesDetailResponseModel.kt
@@ -0,0 +1,11 @@
+package br.com.aramizu.themoviedb.data.model
+
+/**
+ * Model MoviesDetailResponseModel
+ */
+class MoviesDetailResponseModel {
+
+    val runtime: Int = 0
+    var genres: ArrayList<Genres>? = null
+
+}
